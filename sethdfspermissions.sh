50b20490d68a1b98c4bf18326b845dc516d1a953
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-07-21 15:55:52 -0400

    Update sethdfspermissions.sh

--- sethdfspermissions.sh
+++ sethdfspermissions.sh
@@ -10,3 +10,6 @@ HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/sensors
 HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/sensors
 HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /user/kafka
 HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/itemprice
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/itemprice
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /tmp/itemprice
